982a53960e7f4e9b16f531e06681ad0220f76d0d
Bella <45954420+bellahenkel@users.noreply.github.com>
2023-10-23 19:49:16 -0500

    Update co2_sensor.cpp
    This is the updated CO2 sensor code. Updates include:
    
    -I cleaned it up quite a bit and added some comments for readability.
    
    -updated the formula used to convert the voltage drop from the sensor to ppm of CO2. It should be more accurate with this newer formula.
    
    -CO2 data now saves to the SD card with timestamp.

--- CO2-Code/co2_sensor.cpp
+++ CO2-Code/co2_sensor.cpp
@@ -9,7 +9,7 @@
 
 //Includes the libraries for the BME280 sensor
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+//#include <Adafruit_BME280.h>
 
 //Includes the library for the SD card
 #include <SD.h>
@@ -17,11 +17,14 @@
 //Includes the Library for the Real Time Clock
 #include <DS3231.h>
 
+//Defines Sea Level Pressure for BME280
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 //Adds Definitions to the SD pin and File info for SD card
 #define SD_SS_PIN 12
 #define FILE_NAME "Test1.txt"
 #define LOGGERNAME "Mayfly Soil Vapor Sensor"
-#define DATA_HEADER "Date,Time,Solenoid Number,Heater State,Water Level Measurement,O2(%),O2 Volt,CO2(PPM),CO2 Volt,CH4(PPM),CH4 Volt,Ambient Temperature (C),Ambient Humidity (%), Ambient Pressure (hPa), Temperature (C), Humidity (%), Pressure (hPa)"
+#define DATA_HEADER "Date,Time,Solenoid Number,CO2(PPM),CO2 Volt"
 
 //Creates Clock Variable for RTC
 DS3231 Clock;
@@ -30,21 +33,38 @@ DS3231 Clock;
 bool h24;
 bool PM;
 bool Century;
-String sensor;
-const int chipSelect = 10;
 int second, minute, hour, month, DayMonth, year;
 
-//Creates variables for different ADCs
+//Creates variables for ADC
 Adafruit_ADS1115 ads1;
-Adafruit_ADS1115 ads2;
 
 //Setting variables for the ADC results
 int16_t results2;
-float multiplier = 0.125F;
 float CO2_Volt;
 float CO2_PPM;
-float accuracy_modifer;
 
+
+void setupLogFile() {
+  //Checks if SD Card is missing
+  if (!SD.begin(SD_SS_PIN)) {
+    Serial.println("Error: SD card failed to initialise or is missing.");
+  }
+
+  //Check if the file already exists
+  bool oldFile = SD.exists(FILE_NAME);
+
+  //Open the file in write mode
+  File logFile = SD.open(FILE_NAME, FILE_WRITE);
+
+  //Add header information if the file did not already exist
+  if (!oldFile) {
+    logFile.println(LOGGERNAME);
+    logFile.println(DATA_HEADER);
+  }
+
+  //Close the file to save it
+  logFile.close();
+}
 
 String createDataRecord() {
   //Requests data from RTC
@@ -74,7 +94,6 @@ String createDataRecord() {
   return data;
 }
 
-//Creates function to save data in a string
 void logData(String rec) {
   //Re-open the file
   File logFile = SD.open(FILE_NAME, FILE_WRITE);
@@ -86,111 +105,79 @@ void logData(String rec) {
   logFile.close();
 }
 
-//Initialise the SD card and creates file if not there
-void setupLogFile() {
-  //Checks if SD Card is missing
-  if (!SD.begin(SD_SS_PIN)) {
-    Serial.println("Error: SD card failed to initialise or is missing.");
-  }
 
-  //Check if the file already exists
-  bool oldFile = SD.exists(FILE_NAME);
-
-  //Open the file in write mode
-  File logFile = SD.open(FILE_NAME, FILE_WRITE);
-
-  //Add header information if the file did not already exist
-  if (!oldFile) {
-    logFile.println(LOGGERNAME);
-    logFile.println(DATA_HEADER);
-  }
-
-  //Close the file to save it
-  logFile.close();
-}
 
 void setup() {
 
   Wire.begin();
-
   //Begins serial communication with 9600 Baud
   Serial.begin(9600);
 
-  //Set the gain for each ADC located on AA0,AA1, AA2, and AA3 on Mayfly Microcontroller
+    //Begins serial communication with 9600 Baud
+  Serial.println("Setting ads variables...");
+  //Set the gain for ADC located on AA1 on Mayfly Microcontroller
   ads1.setGain(GAIN_ONE);  // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  //Set the gain for ADS1115 ADC
-  ads2.setGain(GAIN_ONE);  // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+
+  Serial.println("ADS variable setup complete");
 
   //Check if ADC on mayfly is operational at 0x48 address
+  Serial.println("Checking if ads1 is initialized...");
   if (!ads1.begin(0x48)) {
     Serial.println("Failed to initialize ADS1.");
-    while (1)
-      ;
-  }
-
-
-  //Check if ADS1115 ADC is operational at 0x49 address
-  if (!ads2.begin(0x49)) {
-    Serial.println("Failed to initialize ADS2.");
     while (1);
   }
-
-  //Initialise log file
-  setupLogFile();
+  Serial.println("Initialization complete.");
+  
+  setupLogFile(); 
 
 }
 
 void loop() {
 
+  //voltage difference across AA2 and AA3 from CO2 sensor
+  results2 = ads1.readADC_Differential_2_3();
+ 
+  //determines voltage value taken from sensor, converts to amps
+  CO2_Volt = ads1.computeVolts(results2);
+  float CO2_Amp = CO2_Volt/250 * 1000;
 
-    // Read Oxygen and CO2 results from the Mayfly ADCs
-    results2 = ads1.readADC_Differential_2_3();
+  //converts voltage value to ppm (recommended by sensor manufacturer when reading voltage across 250ohm resistor)
+  CO2_PPM = 312.5 * CO2_Amp - 1250;
+
+  //the amount of time the Mayfly waits between taking readings (defaults to 10 seconds)
+  const int wait_time = 10000;
+
+  //establishes RTC values for serial monitor
+  hour = Clock.getHour(h24, PM);
+  minute = Clock.getMinute();
+  second = Clock.getSecond();
+
+
+  //prints values to serial monitor with timestamp
+  Serial.print(hour);
+  Serial.print(':');
+  Serial.print(minute);
+  Serial.print(':');
+  Serial.print(second);
+  Serial.print(' ');
+
+  Serial.print(" CO2 = ");
+  Serial.print(CO2_PPM);
+  Serial.print("PPM, ");
+
+  Serial.print(" CO2 Voltage = ");
+  Serial.print(CO2_Volt);
+  Serial.println("V ");
+
+
+  //sends data to sd card
+  String dataRec = createDataRecord();
+
+  logData(dataRec);
+  delay(wait_time);
   
-    //Compute voltage from ADC results
-    CO2_Volt = results2 * multiplier / 1000;
-
-    //_______________Algorithm Computation_______________________________________
-
-
-    //Converts Volts CO2 to PPM
-    //CO2_PPM = (1671.4 * abs(CO2_Volt) - 1287.3);
-    //CO2_PPM = (1671.4 * abs(CO2_Volt) - 1287.3);
-    //CO2_PPM = (-178.56 * pow(abs(CO2_Volt),2)) + (2608.7 * abs(CO2_Volt)) - 2297.3;
-    // CO2_PPM = 3746.1 * log(abs(CO2_Volt)) - 52.951;
-    CO2_PPM = (687.33 * pow(abs(CO2_Volt),3)) - (4334.2 * pow(abs(CO2_Volt),2)) + (9383.9 * abs(CO2_Volt)) - 5605.8;
-
-    //Error Reading if negative value
-    if (CO2_PPM <= 0) {
-      CO2_PPM = -999;
-    }
-
-    // Creates data string from function
-    String dataRec = createDataRecord();
-
-    //Display Measurements to Serial Monitor
-    Serial.println("-----------------------------------------------------------------------------");
-
-
-    Serial.print("CO2 = ");
-    Serial.print(CO2_Volt,3);
-    Serial.print("V, ");
-
-    Serial.print("CO2 = ");
-    Serial.print(CO2_PPM);
-    Serial.print("PPM, ");
-
-
-    Serial.print("\n");
-    Serial.println("-----------------------------------------------------------------------------");
-    //End Barrier for serial monitor
-
-
-    //Save the data record to the log file
-    logData(dataRec);
-
-    delay(1000);
-  
-}
+    
+  }
 
 
 
